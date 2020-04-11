9badfc396cfd245e73cea2e2b02e919c28b7ca65
Koundinya Srinivasarao <ksrinivasarao@pivotal.io>
2020-04-11 01:12:54 -0400

    Use single quote for password fields as you can avoid escaping special characters in passwords.

--- env-files/tas-exports.sh
+++ env-files/tas-exports.sh
@@ -1,7 +1,7 @@
 export YTT_TAS_system_registry__hostname="registry.pivotal.io"
 export YTT_TAS_system_registry__username="USERID"
-export YTT_TAS_system_registry__password="PASSWORD"
+export YTT_TAS_system_registry__password='PASSWORD'
 export YTT_TAS_app_registry__hostname="harbor.<YOUR-DOMAIN>"
 export YTT_TAS_app_registry__repository="harbor.<YOUR-DOMAIN>/tas-workloads"
 export YTT_TAS_app_registry__username="admin"
-export YTT_TAS_app_registry__password="<HARBOR-PASSWORD>"
+export YTT_TAS_app_registry__password='<HARBOR-PASSWORD>'
